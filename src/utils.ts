a1454c75d7c825bf207e8f7758cef593272bdcfd
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2026-06-07 22:05:40 +1000

    only query for tcp devices via bonjour until companion can pass on details to connection

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,7 @@ export function handleBonjourHost(config: ModuleConfig): ModuleConfig {
 	if (config.bonjourHost) {
 		config.host = config.bonjourHost.split(':')[0]
 		config.port = Number.parseInt(config.bonjourHost.split(':')[1]) || 65000
+		config.protocol = 'tcp' // Bonjour only supports TCP
 		utilsLogger.info(`Bonjour device selected: ${config.host}:${config.port}`)
 	}
 	return config
