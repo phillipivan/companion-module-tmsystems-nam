cdb4c065f037a1c8fbd4db07bd90449b378ac99d
Phillip Ivan Pietruschka <ivanpietruschka@gmail.com>
2026-06-12 22:16:38 +1000

    add variables logger

--- src/variables.ts
+++ src/variables.ts
@@ -1,5 +1,8 @@
+import { createModuleLogger } from '@companion-module/base'
 import type ModuleInstance from './main.js'
 
 export function UpdateVariableDefinitions(self: ModuleInstance): void {
+	const logger = createModuleLogger('OCA Variables')
+	logger.debug('Updating variables')
 	self.setVariableDefinitions({})
 }
